751f672f5c6b4378bcb339e2187612f308cc3abf
Yash Thakkar <yashthakkar911@gmail.com>
2022-09-25 22:26:28 -0400

    Splash screen

---
+++ app/src/main/java/com/example/notesapp/BaseViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.notesapp
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+open class BaseViewModel(application: Application) : AndroidViewModel(application) {
+
+}
