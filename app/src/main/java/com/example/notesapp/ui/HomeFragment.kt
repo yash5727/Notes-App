5e49487024888fa5775ec54beeaa4f18748fa367
pratik <pratik.sheth@havas.com>
2022-10-09 15:10:20 -0400

    no message

--- app/src/main/java/com/example/notesapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/notesapp/ui/HomeFragment.kt
@@ -11,6 +11,7 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class HomeFragment : Fragment() {
+    
     private var param1: String? = null
     private var param2: String? = null
 
