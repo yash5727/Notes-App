c827658a385f421b711f36bed93e481999b446db
pratik <pratik.sheth@havas.com>
2022-10-10 19:16:57 -0400

    Refractor code

--- app/src/main/java/com/example/notesapp/BaseFragment.kt
+++ app/src/main/java/com/example/notesapp/BaseFragment.kt
@@ -1,30 +1,13 @@
 package com.example.notesapp
 
-import android.view.WindowManager
 import androidx.annotation.LayoutRes
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 
 abstract class BaseFragment(@LayoutRes contentLayoutId: Int) : Fragment(contentLayoutId) {
 
-    companion object {
-        var errorDialog: AlertDialog? = null
-    }
-
     abstract fun setupObservers()
-
-    private fun freezeScreen() {
-        requireActivity().window.setFlags(
-            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
-            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
-        )
-    }
-
-    private fun unfreezeScreen() {
-        requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
-    }
 
     fun navigate(destination: NavDirections) = with(findNavController()) {
         currentDestination?.getAction(destination.actionId)
--- app/src/main/java/com/example/notesapp/ui/lockedNotes/LockedNotesFragment.kt
+++ app/src/main/java/com/example/notesapp/ui/lockedNotes/LockedNotesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.notesapp.ui
+package com.example.notesapp.ui.lockedNotes
 
 import android.os.Bundle
 import android.view.LayoutInflater
